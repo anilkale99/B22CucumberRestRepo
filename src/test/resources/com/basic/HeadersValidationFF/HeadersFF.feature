2790d278e0e01b129bdb936429bb90e4daffdea4
Anil Kale <anilkale.99@gmail.com>
2020-07-26 12:22:27 +0530

    my first commit

---
+++ src/test/resources/com/basic/HeadersValidationFF/HeadersFF.feature
@@ -0,0 +1,8 @@
+Feature: First GET Rest Assured and validate headers test101
+
+
+Scenario: Test my Get with headers Method101
+Given  user start the rest asured test
+Then user checks headers "Content-Type" have "application/json; charset=utf-8" value
+
+
---
+++ target/cucumber-htmlreportB22/report.js
@@ -0,0 +1,513 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("GetRequestTesting.feature");
+formatter.feature({
+  "line": 2,
+  "name": "First GET Rest Assured test105",
+  "description": "",
+  "id": "first-get-rest-assured-test105",
+  "keyword": "Feature",
+  "tags": [
+    {
+      "line": 1,
+      "name": "@Important"
+    }
+  ]
+});
+formatter.scenario({
+  "line": 5,
+  "name": "Test my Get Method111",
+  "description": "",
+  "id": "first-get-rest-assured-test105;test-my-get-method111",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 4,
+      "name": "@Smoke"
+    }
+  ]
+});
+formatter.step({
+  "line": 6,
+  "name": "user start the rest asured test",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 7,
+  "name": "user hit the get request",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 8,
+  "name": "user checks the \"30\" count or size",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 9,
+  "name": "user checks data at path \"MRData.limit\" having value \"30\" in jason",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 10,
+  "name": "user checks data at path \"MRData.CircuitTable.Circuits[0].circuitId\" having value \"albert_park\" in jason",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "GETRequestTestSD.user_start_the_rest_asured_test()"
+});
+formatter.result({
+  "duration": 1944800,
+  "status": "passed"
+});
+formatter.match({
+  "location": "GETRequestTestSD.user_hit_the_get_request()"
+});
+formatter.result({
+  "duration": 708296200,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "30",
+      "offset": 17
+    }
+  ],
+  "location": "GETRequestTestSD.user_checks_the_count_or_size(String)"
+});
+formatter.result({
+  "duration": 20261300,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "MRData.limit",
+      "offset": 26
+    },
+    {
+      "val": "30",
+      "offset": 54
+    }
+  ],
+  "location": "GETRequestTestSD.user_checks_data_at_path_having_value_in_jason(String,String)"
+});
+formatter.result({
+  "duration": 25701900,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "MRData.CircuitTable.Circuits[0].circuitId",
+      "offset": 26
+    },
+    {
+      "val": "albert_park",
+      "offset": 83
+    }
+  ],
+  "location": "GETRequestTestSD.user_checks_data_at_path_having_value_in_jason(String,String)"
+});
+formatter.result({
+  "duration": 47798500,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 13,
+  "name": "Test my Get Method112",
+  "description": "",
+  "id": "first-get-rest-assured-test105;test-my-get-method112",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 12,
+      "name": "@Smoke"
+    },
+    {
+      "line": 12,
+      "name": "@Regression"
+    }
+  ]
+});
+formatter.step({
+  "line": 14,
+  "name": "user start the rest asured test",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 15,
+  "name": "user hit the get request",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 16,
+  "name": "user checks the \"30\" count or size",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 17,
+  "name": "user checks data at path \"MRData.limit\" having value \"30\" in jason",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 18,
+  "name": "user checks data at path \"MRData.CircuitTable.Circuits[0].circuitId\" having value \"albert_park\" in jason",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "GETRequestTestSD.user_start_the_rest_asured_test()"
+});
+formatter.result({
+  "duration": 50500,
+  "status": "passed"
+});
+formatter.match({
+  "location": "GETRequestTestSD.user_hit_the_get_request()"
+});
+formatter.result({
+  "duration": 753077400,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "30",
+      "offset": 17
+    }
+  ],
+  "location": "GETRequestTestSD.user_checks_the_count_or_size(String)"
+});
+formatter.result({
+  "duration": 64890400,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "MRData.limit",
+      "offset": 26
+    },
+    {
+      "val": "30",
+      "offset": 54
+    }
+  ],
+  "location": "GETRequestTestSD.user_checks_data_at_path_having_value_in_jason(String,String)"
+});
+formatter.result({
+  "duration": 38319700,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "MRData.CircuitTable.Circuits[0].circuitId",
+      "offset": 26
+    },
+    {
+      "val": "albert_park",
+      "offset": 83
+    }
+  ],
+  "location": "GETRequestTestSD.user_checks_data_at_path_having_value_in_jason(String,String)"
+});
+formatter.result({
+  "duration": 39247500,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 21,
+  "name": "Test my Get Method113",
+  "description": "",
+  "id": "first-get-rest-assured-test105;test-my-get-method113",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 20,
+      "name": "@Regression"
+    }
+  ]
+});
+formatter.step({
+  "line": 22,
+  "name": "user start the rest asured test",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 23,
+  "name": "user hit the get request",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 24,
+  "name": "user checks the \"30\" count or size",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 25,
+  "name": "user checks data at path \"MRData.limit\" having value \"30\" in jason",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 26,
+  "name": "user checks data at path \"MRData.CircuitTable.Circuits[0].circuitId\" having value \"albert_park\" in jason",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "GETRequestTestSD.user_start_the_rest_asured_test()"
+});
+formatter.result({
+  "duration": 111600,
+  "status": "passed"
+});
+formatter.match({
+  "location": "GETRequestTestSD.user_hit_the_get_request()"
+});
+formatter.result({
+  "duration": 754993600,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "30",
+      "offset": 17
+    }
+  ],
+  "location": "GETRequestTestSD.user_checks_the_count_or_size(String)"
+});
+formatter.result({
+  "duration": 43042800,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "MRData.limit",
+      "offset": 26
+    },
+    {
+      "val": "30",
+      "offset": 54
+    }
+  ],
+  "location": "GETRequestTestSD.user_checks_data_at_path_having_value_in_jason(String,String)"
+});
+formatter.result({
+  "duration": 33672300,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "MRData.CircuitTable.Circuits[0].circuitId",
+      "offset": 26
+    },
+    {
+      "val": "albert_park",
+      "offset": 83
+    }
+  ],
+  "location": "GETRequestTestSD.user_checks_data_at_path_having_value_in_jason(String,String)"
+});
+formatter.result({
+  "duration": 63892000,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 29,
+  "name": "Test my Get Method114",
+  "description": "",
+  "id": "first-get-rest-assured-test105;test-my-get-method114",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 30,
+  "name": "user start the rest asured test",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 31,
+  "name": "user hit the get request",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 32,
+  "name": "user checks the \"30\" count or size",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 33,
+  "name": "user checks data at path \"MRData.limit\" having value \"30\" in jason",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 34,
+  "name": "user checks data at path \"MRData.CircuitTable.Circuits[0].circuitId\" having value \"albert_park\" in jason",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "GETRequestTestSD.user_start_the_rest_asured_test()"
+});
+formatter.result({
+  "duration": 264200,
+  "status": "passed"
+});
+formatter.match({
+  "location": "GETRequestTestSD.user_hit_the_get_request()"
+});
+formatter.result({
+  "duration": 857924100,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "30",
+      "offset": 17
+    }
+  ],
+  "location": "GETRequestTestSD.user_checks_the_count_or_size(String)"
+});
+formatter.result({
+  "duration": 52429900,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "MRData.limit",
+      "offset": 26
+    },
+    {
+      "val": "30",
+      "offset": 54
+    }
+  ],
+  "location": "GETRequestTestSD.user_checks_data_at_path_having_value_in_jason(String,String)"
+});
+formatter.result({
+  "duration": 23174900,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "MRData.CircuitTable.Circuits[0].circuitId",
+      "offset": 26
+    },
+    {
+      "val": "albert_park",
+      "offset": 83
+    }
+  ],
+  "location": "GETRequestTestSD.user_checks_data_at_path_having_value_in_jason(String,String)"
+});
+formatter.result({
+  "duration": 28009000,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 37,
+  "name": "Test my Get Method115",
+  "description": "",
+  "id": "first-get-rest-assured-test105;test-my-get-method115",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 36,
+      "name": "@Regression"
+    }
+  ]
+});
+formatter.step({
+  "line": 38,
+  "name": "user start the rest asured test",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 39,
+  "name": "user hit the get request",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 40,
+  "name": "user checks the \"30\" count or size",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 41,
+  "name": "user checks data at path \"MRData.limit\" having value \"30\" in jason",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 42,
+  "name": "user checks data at path \"MRData.CircuitTable.Circuits[0].circuitId\" having value \"albert_park\" in jason",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "GETRequestTestSD.user_start_the_rest_asured_test()"
+});
+formatter.result({
+  "duration": 69600,
+  "status": "passed"
+});
+formatter.match({
+  "location": "GETRequestTestSD.user_hit_the_get_request()"
+});
+formatter.result({
+  "duration": 802891000,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "30",
+      "offset": 17
+    }
+  ],
+  "location": "GETRequestTestSD.user_checks_the_count_or_size(String)"
+});
+formatter.result({
+  "duration": 20625700,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "MRData.limit",
+      "offset": 26
+    },
+    {
+      "val": "30",
+      "offset": 54
+    }
+  ],
+  "location": "GETRequestTestSD.user_checks_data_at_path_having_value_in_jason(String,String)"
+});
+formatter.result({
+  "duration": 20249400,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "MRData.CircuitTable.Circuits[0].circuitId",
+      "offset": 26
+    },
+    {
+      "val": "albert_park",
+      "offset": 83
+    }
+  ],
+  "location": "GETRequestTestSD.user_checks_data_at_path_having_value_in_jason(String,String)"
+});
+formatter.result({
+  "duration": 69133600,
+  "status": "passed"
+});
+});
